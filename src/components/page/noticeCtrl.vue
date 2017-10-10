1c07e18ed86b18da32d7dc5d3ca759d5437dd80f
YangMin <2577441757@qq.com>
2017-10-10 17:50:46 +0800

    ''

--- src/components/page/noticeCtrl.vue
+++ src/components/page/noticeCtrl.vue
@@ -79,6 +79,15 @@
 	      prop="handle"
 	      label="操作"
 	      min-width="120">
+	       <template scope="scope">
+		        <el-button
+		          size="small"
+		          @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+		        <el-button
+		          size="small"
+		          type="danger"
+		          @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+	      </template>
 	    </el-table-column>
 	  </el-table>
 	</div>
